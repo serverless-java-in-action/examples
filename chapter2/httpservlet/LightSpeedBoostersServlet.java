65db6c2e3bbd2c00e898164fabb047dc1326baa8
Daniel Oh <doh@redhat.com>
2023-09-26 23:26:22 -0400

    Add Quarkus REST example

--- chapter2/httpservlet/LightSpeedBoostersServlet.java
+++ chapter2/httpservlet/LightSpeedBoostersServlet.java
@@ -1,4 +1,4 @@
-package chapter2;
+package chapter2.httpservlet;
 
 @WebServlet("/api/getLSBData")
 public class LightSpeedBoostersServlet extends HttpServlet {
--- chapter2/springboot/LightSpeedBoostersController.java
+++ chapter2/springboot/LightSpeedBoostersController.java
@@ -1,4 +1,4 @@
-package chapter2;
+package chapter2.springboot;
 
 @RestController
 @RequestMapping("/api")
