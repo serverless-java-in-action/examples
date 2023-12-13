c2a8cf15e518701bda8031ffbcb9cea08eee0772
Daniel Oh <doh@redhat.com>
2023-12-12 19:34:56 -0500

    Update gcloud functions

---
+++ chapter3/quarkus-google-cloud-functions-http/src/main/java/org/acme/LightSpeedBoosterResource.java
@@ -0,0 +1,39 @@
+package org.acme;
+
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("/")
+public class LightSpeedBoosterResource {
+
+    @GET
+    @Path("/getLSBData")
+    public List<LightSpeedBooster> listAll() {
+        return LightSpeedBooster.listAll();
+    }
+
+    @GET
+    @Path("/getWarpDrive")
+    public List<LightSpeedBooster> findByWarpDrive() {
+        return LightSpeedBooster.findWarpDrive();
+    }
+
+    /* 
+    curl -X POST http://localhost:8080/api -v \
+        -H 'Content-Type: application/json' \
+        -d '{"name":"Hyperspace Jumper","quantity":1,"credit":1000000,"isWarpDrive":true}' 
+   */
+    @POST
+    @Transactional
+    public Response addNewPart(LightSpeedBooster lightSpeedBooster) {
+        lightSpeedBooster.persist();
+        return Response.status(Status.CREATED).entity(lightSpeedBooster).build();
+    }
+
+}
