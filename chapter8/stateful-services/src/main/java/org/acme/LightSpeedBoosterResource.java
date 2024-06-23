024d29a1108a00d053aaab08574031a4f8a43bb5
Daniel Oh <doh@redhat.com>
2024-06-23 17:37:50 -0400

    Update stateful services

--- chapter8/stateful-services/src/main/java/org/acme/LightSpeedBoosterResource.java
+++ chapter8/stateful-services/src/main/java/org/acme/LightSpeedBoosterResource.java
@@ -1,36 +1,17 @@
 package org.acme;
 
-// import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-// import jakarta.ws.rs.Produces;
-// import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 
 @Path("/api")
 public class LightSpeedBoosterResource {
 
-    // Without PostgreSQL
-    // @GET
-    // @Produces(MediaType.TEXT_PLAIN)
-    // @Path("/getLSBData")
-    // public List<String> listAll() {
-
-    //     List<String> lightSpeedBooster = new ArrayList<String>();
-    //     lightSpeedBooster.add("Tachyon Burst");
-    //     lightSpeedBooster.add("Thrusters");
-    //     lightSpeedBooster.add("Transwarp");
-    //     lightSpeedBooster.add("Turbolift");
-    //     return lightSpeedBooster;
-
-    // }
-
-    // With PostgreSQL
     @GET
     @Path("/getLSBData")
     public List<LightSpeedBooster> listAll() {
