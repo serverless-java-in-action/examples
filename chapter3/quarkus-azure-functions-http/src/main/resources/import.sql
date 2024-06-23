fce67daf8a866a93e96588a1df91de6fd262050f
Daniel Oh <doh@redhat.com>
2024-06-23 17:52:13 -0400

    remove postgres for stateless functions

--- chapter3/quarkus-azure-functions-http/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO LightSpeedBooster (id, name, quantity, credit, isWarpDrive) VALUES (1, 'Tachyon Burst', 100, 300, false);
-INSERT INTO LightSpeedBooster (id, name, quantity, credit, isWarpDrive) VALUES (2, 'Thrusters', 200, 250, false);
-INSERT INTO LightSpeedBooster (id, name, quantity, credit, isWarpDrive) VALUES (3, 'Transwarp', 10, 5000, true);
-INSERT INTO LightSpeedBooster (id, name, quantity, credit, isWarpDrive) VALUES (4, 'Turbolift', 300, 700, false);
-ALTER SEQUENCE LightSpeedBooster_SEQ RESTART WITH 5;
--- chapter3/quarkus-google-cloud-functions-http/src/main/java/org/acme/LightSpeedBoosterResource.java
+++ chapter3/quarkus-google-cloud-functions-http/src/main/java/org/acme/LightSpeedBoosterResource.java
@@ -1,39 +1,28 @@
 package org.acme;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/api")
 public class LightSpeedBoosterResource {
 
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     @Path("/getLSBData")
-    public List<LightSpeedBooster> listAll() {
-        return LightSpeedBooster.listAll();
-    }
+    public List<String> listAll() {
 
-    @GET
-    @Path("/getWarpDrive")
-    public List<LightSpeedBooster> findByWarpDrive() {
-        return LightSpeedBooster.findWarpDrive();
-    }
+        List<String> lightSpeedBooster = new ArrayList<String>();
+        lightSpeedBooster.add("Tachyon Burst");
+        lightSpeedBooster.add("Thrusters");
+        lightSpeedBooster.add("Transwarp");
+        lightSpeedBooster.add("Turbolift");
+        return lightSpeedBooster;
 
-    /* 
-    curl -X POST http://localhost:8080/api -v \
-        -H 'Content-Type: application/json' \
-        -d '{"name":"Hyperspace Jumper","quantity":1,"credit":1000000,"isWarpDrive":true}' 
-   */
-    @POST
-    @Transactional
-    public Response addNewPart(LightSpeedBooster lightSpeedBooster) {
-        lightSpeedBooster.persist();
-        return Response.status(Status.CREATED).entity(lightSpeedBooster).build();
     }
 
 }
