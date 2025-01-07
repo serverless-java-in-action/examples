f0769790b44d37b8a2ca4b146b4d6980f5ad13c9
Kevin Dubois <kevin.dubois@gmail.com>
2025-01-07 15:19:37 +0100

    Add Chapter 5 containerized-quarkus-app

---
+++ chapter5/containerized-quarkus-app/src/test/java/org/acme/SpaceDeliveryResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class SpaceDeliveryResourceIT extends SpaceDeliveryResourceTest {
+    // Execute the same tests but in packaged mode.
+}
